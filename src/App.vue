dadf39d0757f4aeede18d4a0c88839da29932672
RamanSlinka <romuch1986@gmail.com>
2021-11-19 01:15:37 +0300

    added modal; added axios - got posts

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="app">
-    <post-form
-        @create="createPost"
-    />
+    <h2>Posts page</h2>
+    <my-button
+        style="margin: 15px 0"
+        @click="showDialog"
+    >Create post
+    </my-button>
+
+    <my-button
+        @click="fetchPosts"
+    >Get posts
+    </my-button>
+
+    <my-dialog v-model="dialogVisible">
+      //v-model:show="dialogVisible"
+      <post-form
+          @create="createPost"
+      />
+    </my-dialog>
+
     <post-list :posts="posts"
                @remove="removePost"
+               v-if="!isPostLoading"
     />
-
+    <div v-else>Loading...</div>
   </div>
 </template>
 
@@ -14,31 +31,52 @@
 
 import PostForm from "@/components/PostForm";
 import PostList from "@/components/PostList";
+import axios from "axios"
+import MyButton from "@/components/UI/MyButton";
 
 export default {
   components: {
+    MyButton,
     PostList, PostForm
   },
 
 
   data() {
     return {
-      posts: [
-        {id: 1, title: 'JavaScript', body: 'Description post'},
-        {id: 2, title: 'TypeScript', body: 'Description post 2'},
-        {id: 3, title: 'React', body: 'Description post 3'}
-      ],
-
+      posts: [],
+      dialogVisible: false,
+      isPostLoading: false,
     }
   },
   methods: {
     createPost(post) {
-      this.posts.push(post)
+      this.posts.push(post);
+      this.dialogVisible = false;
+
     },
     removePost(post) {
-      this.posts =  this.posts.filter(p => p.id !== post.id)
-    }
+      this.posts = this.posts.filter(p => p.id !== post.id)
+    },
+    showDialog() {
+      this.dialogVisible = true
+    },
+    async fetchPosts() {
+      try {
+        this.isPostLoading = true;
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+        this.posts = response.data;
 
+      } catch (e) {
+        alert('error')
+      }
+      finally {
+        this.isPostLoading = false;
+      }
+    },
+
+    mounted() {
+      this.fetchPosts();
+    }
   }
 }
 </script>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
+  <div v-if="posts.length > 0 ">
     <h3>List posts</h3>
     <post-item v-for="post in posts"
                :key="post.id"
                :post="post"
                @remove="$emit('remove', post)"
     />
+  </div>
+  <h2 v-else style="color: red">
+    There is no list posts :(
+  </h2>
   </div>
 </template>
 
